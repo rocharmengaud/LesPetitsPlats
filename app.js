8b3da813d476b14b44e37e24a105f18200d74ca8
Roch Armengaud <armengaud.roch@live.fr>
2022-08-02 18:55:19 +0200

    setting up cards in js

--- app.js
+++ app.js
@@ -11,13 +11,64 @@ class Api {
   }
 }
 
+class recipeCard {
+  constructor(card) {
+    // Classe constructeur pour pouvoir utiliser les clés au sein du json
+    this.card = card;
+    // des qu'on crée un constructor, on utilisera this
+  }
+
+  createRecipeCard() {
+    const cardGrid = document.querySelector('.cards-grid');
+
+    const card = document.createElement('div');
+    card.className = 'card';
+    const cardPicture = document.createElement('img');
+    cardPicture.className = 'card-picture';
+
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+
+    const cardRecipe = document.createElement('div');
+    cardRecipe.classList.add('card-recipe', 'flex');
+
+    const cardTitle = document.createElement('div');
+    cardTitle.className = 'card-title';
+    cardTitle.innerHTML = this.card.name;
+    const cardTiming = document.createElement('div');
+    cardTiming.className = 'card-timing';
+    cardTiming.innerHTML = this.card.time + 'minutes';
+
+    const cardInfo = document.createElement('div');
+    cardInfo.classList.add('card-info', 'flex');
+
+    const cardIngredients = document.createElement('div');
+    cardIngredients.className = 'card-ingredients';
+    cardIngredients.innerHTML = this.card.ingredient;
+    const cardDescription = document.createElement('div');
+    cardDescription.className = 'card-description';
+
+    cardGrid.appendChild(card);
+    card.appendChild(cardPicture);
+    card.appendChild(cardBody);
+    cardBody.appendChild(cardRecipe);
+    cardRecipe.appendChild(cardTitle);
+    cardRecipe.appendChild(cardTiming);
+  }
+}
+
 class App {
   constructor() {
     this.fullData = new Api('data/recipes.json');
   }
 
   async main() {
-    console.log('test');
+    const json = await this.fullData.get();
+    for (const card of json.recipes) {
+      // json.recipes = json.la clé.dans le json (ici "recipes")
+      const template = new recipeCard(card);
+      template.createRecipeCard();
+    }
   }
 }
 
