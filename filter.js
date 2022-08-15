bf020f44547e214cecbe14d33d154d2c102a07d3
Roch Armengaud <armengaud.roch@live.fr>
2022-08-15 18:25:07 +0200

    tag removing effective

--- filter.js
+++ filter.js
@@ -1,3 +1,16 @@
+class Api {
+  constructor(url) {
+    this.url = url;
+  }
+
+  // méthode asynchrone pour initialiser la recupération des données du json
+  async get() {
+    const httpResponse = await fetch(this.url);
+    const httpData = await httpResponse.json();
+    return httpData;
+  }
+}
+
 import { getData } from './data.js';
 const recipesAll = await getData();
 
@@ -7,7 +20,6 @@ const ingredientListElements = document.querySelectorAll('.ingredient-list div')
 const applianceListElements = document.querySelectorAll('.appliance-list div');
 const ustensilsListElements = document.querySelectorAll('.ustensils-list div');
 const tagsContainer = document.querySelector('.tags-container');
-const tag = document.querySelectorAll('.tag');
 
 /* addeventlistener on the search input */
 searchbarInput.addEventListener('change', function (event) {
@@ -16,9 +28,11 @@ searchbarInput.addEventListener('change', function (event) {
 
 /* addeventlistener on insert tag element */
 Array.from(ingredientListElements).forEach(function (element) {
+  const tagId = element.innerHTML;
   element.addEventListener('click', function (event) {
     const ingredientTag = document.createElement('button');
     ingredientTag.className = 'btn btn-primary tag blue';
+    ingredientTag.setAttribute('id', element.innerHTML);
     const tagContent = document.createElement('div');
     tagContent.className = 'tag-content flex';
     const closeTag = document.createElement('img');
@@ -28,13 +42,20 @@ Array.from(ingredientListElements).forEach(function (element) {
     ingredientTag.appendChild(tagContent);
     tagContent.appendChild(element);
     tagContent.appendChild(closeTag);
+
+    closeTag.addEventListener('click', function (event) {
+      const tag = document.getElementById(tagId);
+      tag.remove();
+    });
   });
 });
 
 Array.from(applianceListElements).forEach(function (element) {
+  const tagId = element.innerHTML;
   element.addEventListener('click', function (event) {
     const applianceTag = document.createElement('button');
     applianceTag.className = 'btn btn-primary tag green';
+    applianceTag.setAttribute('id', element.innerHTML);
     const tagContent = document.createElement('div');
     tagContent.className = 'tag-content flex';
     const closeTag = document.createElement('img');
@@ -44,13 +65,20 @@ Array.from(applianceListElements).forEach(function (element) {
     applianceTag.appendChild(tagContent);
     tagContent.appendChild(element);
     tagContent.appendChild(closeTag);
+
+    closeTag.addEventListener('click', function (event) {
+      const tag = document.getElementById(tagId);
+      tag.remove();
+    });
   });
 });
 
 Array.from(ustensilsListElements).forEach(function (element) {
+  const tagId = element.innerHTML;
   element.addEventListener('click', function (event) {
     const ustensilsTag = document.createElement('button');
     ustensilsTag.className = 'btn btn-primary tag orange';
+    ustensilsTag.setAttribute('id', element.innerHTML);
     const tagContent = document.createElement('div');
     tagContent.className = 'tag-content flex';
     const closeTag = document.createElement('img');
@@ -60,18 +88,33 @@ Array.from(ustensilsListElements).forEach(function (element) {
     ustensilsTag.appendChild(tagContent);
     tagContent.appendChild(element);
     tagContent.appendChild(closeTag);
+
+    closeTag.addEventListener('click', function (event) {
+      const tag = document.getElementById(tagId);
+      tag.remove();
+    });
   });
 });
 
-/* addeventlistener to delete tag element */
-Array.from(tag).forEach(function (element) {
-  element.addEventListener('click', function (event) {
-    console.log('test');
-  });
-});
+// Object.entries(recipesAll).forEach(([clé, valeur]) => console.log(clé, valeur));
+console.log(Object.values(recipesAll));
+
+// recipesAll.forEach((element) => {});
+
+class recipesData {
+  constructor(data) {
+    // Classe constructeur pour pouvoir utiliser les clés au sein du json
+    this.data = data;
+    // des qu'on crée un constructor, on utilisera this
+  }
+
+  parseJson() {
+    console.log(this.data.name);
+  }
+}
 
 function generalFilter(userInput) {
-  console.log(userInput, recipesAll);
+  // console.log(userInput, recipesAll);
 
   let recipesFiltered = []; // array of recipes filtered by search terms
 
@@ -92,3 +135,22 @@ function generalFilter(userInput) {
 }
 
 generalFilter(searchbarValue);
+
+class App {
+  constructor() {
+    this.fullData = new Api('data/recipes.json');
+  }
+
+  async main() {
+    const json = await this.fullData.get();
+
+    for (const data of json.recipes) {
+      // json.recipes = json.la clé.dans le json (ici "recipes")
+      const template = new recipesData(data);
+      template.parseJson;
+    }
+  }
+}
+
+const app = new App();
+app.main();
