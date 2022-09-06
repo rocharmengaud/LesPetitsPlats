21fe2dbc480fb905484e7b79e574db9118c5504e
Roch Armengaud <armengaud.roch@live.fr>
2022-09-06 21:41:22 +0200

    wip new filtering

--- newFilter.js
+++ newFilter.js
@@ -1,85 +1,63 @@
 import { getData } from './data.js';
 const recipesAll = await getData();
+let tagIngredients = [
+  {
+    ingredient: 'Lait de coco',
+    quantity: 400,
+    unit: 'ml',
+  },
+];
+const tagsContainer = document.querySelector('.tags-container');
 
 function getIngredients(recipes) {
   let ingredients = [];
 
-  for (const recipe of recipesAll.recipes) {
+  for (const recipe of recipes) {
     for (const ingredient of recipe.ingredients) {
-      // console.log(recipe.ingredients);
-      // console.log(ingredient.name);
-      if (!Boolean(ingredients.find((i) => i.name === ingredient.name))) {
+      if (!Boolean(ingredients.find((i) => i.ingredient === ingredient.ingredient))) {
         ingredients.push(ingredient);
       }
     }
-    return ingredients;
+  }
+  return ingredients;
+}
+
+function removeTagIngredient(ingredient) {
+  tagIngredients = tagIngredients.filter((tagIngredient) => ingredient.ingredient !== tagIngredient.ingredient);
+}
+
+function refreshTagIngredientsElement(ingredients) {
+  tagsContainer.innerHTML = '';
+  for (const tagIngredient of ingredients) {
+    const ingredientTag = document.createElement('button');
+    ingredientTag.className = 'btn btn-primary tag blue';
+    const tagContent = document.createElement('div');
+    tagContent.className = 'tag-content flex';
+    const closeTag = document.createElement('img');
+    closeTag.src = './assets/x-circle.svg';
+    closeTag.className = 'close-tag';
+    tagsContainer.appendChild(ingredientTag);
+    ingredientTag.appendChild(tagContent);
+    tagContent.append(tagIngredient.ingredient);
+    tagContent.appendChild(closeTag);
+
+    closeTag.addEventListener('click', (event) => {
+      removeTagIngredient(tagIngredient);
+      // Cette fonction supprime l'element car il n'existe plus quand on repasse dans le for (const tagIngredient of ingredients)
+      refreshTagIngredientsElement(tagIngredients);
+    });
   }
 }
 
-const ingredients = getIngredients(recipesAll);
-console.log(ingredients);
+const ingredients = getIngredients(recipesAll.recipes);
+const ingredientListElements = document.querySelectorAll('.ingredient-list div');
+const applianceListElements = document.querySelectorAll('.appliance-list div');
+const ustensilsListElements = document.querySelectorAll('.ustensils-list div');
+console.log(ingredientListElements);
 
-let targetIngredients = [];
-
-let searchValue = '';
-const searchbarInput = document.querySelector('#search-recipe');
-const searchButton = document.querySelector('#icon');
-const targetIngredientList = document.querySelector('.tags-container');
-const ingredientList = document.querySelectorAll('.ingredient-list');
-const tagsContainer = document.querySelector('.tags-container');
-
-function init() {
-  for (const ingredient of ingredients) {
-    addIngredient(ingredient);
-  }
-}
-
-function addIngredient(ingredient) {
-  const ingredientButton = document.createElement('button');
-  ingredientButton.append(ingredient.name);
-  ingredientButton.addEventListener('click', (e) => {
-    addTargetIngredient(ingredient);
+Array.from(ingredientListElements).forEach(function (element) {
+  element.addEventListener('click', function (event) {
+    // tagIngredients = [???]
+    refreshTagIngredientsElement(tagIngredients);
   });
-  ingredientList.append(ingredientButton);
-}
-
-function addTargetIngredient(ingredient) {
-  if (targetIngredients.includes(ingredient)) return;
-
-  const span = document.createElement('span');
-  const closeButton = document.createElement('button');
-  closeButton.append('x');
-  closeButton.addEventListener('click', (e) => {
-    removeTargetIngredient(ingredient);
-    addIngredient(ingredient);
-  });
-
-  span.append(ingredient.name);
-  span.appendChild(closeButton);
-  targetIngredientList.append(span);
-  targetIngredients.push(ingredient);
-
-  for (const button of ingredientList.childNodes) {
-    const text = button.firstChild.data;
-    if (text === ingredient.name) {
-      button.remove();
-    }
-  }
-}
-
-function removeTargetIngredient(ingredient) {
-  for (const span of targetIngredientList.childNodes) {
-    const text = span.firstChild.data;
-    if (text === ingredient.name) {
-      span.remove();
-    }
-  }
-  targetIngredients = targetIngredients.filter((tIngredient) => ingredient !== tIngredient);
-}
-
-init();
-
-searchButton.addEventListener('click', (e) => {
-  searchValue = searchbarInput.value;
-  console.log(searchbarInput.value);
 });
